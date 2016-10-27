0210129bff08b9f5ef472447eb274470c1bcc460
eudaemonal <eudaemonal@gmail.com>
2016-10-28 10:20:44 +1100

    update

--- README.sh
+++ README.sh
@@ -1,6 +1,6 @@
 
-
 # COMP2041
+# 
 #================================
 Lecture Notes
 Bash command flags (sort, grep ...)
@@ -11,4 +11,41 @@ Bash command flags (sort, grep ...)
 
 
 
+# REVIEW
+#===========================
 
+
+#---------------------------
+
+# Good: 
+
+# Unsolved: 
+
+
+# Unattempted: 
+
+
+#===========================
+# Lab 8 
+
+
+
+#---------------------------
+# Tut 8
+
+# Good: 
+10 read and write files
+15, 16 map and grep
+
+# Unsolved: 
+8 error on find files
+
+# Unattempted: 
+11
+12
+13
+14
+
+
+
+#===========================
