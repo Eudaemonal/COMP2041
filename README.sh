3179bbc573b4ab601ee0a55a502e7b0e89d3c9a6
eudaemonal <eudaemonal@gmail.com>
2016-10-31 09:39:41 +1100

    update

--- README.sh
+++ README.sh
@@ -30,7 +30,78 @@ python 	findall, match
 # Unattempted: 
 
 #===========================
+# Lab 3
+
+declare MediumFiles
+for f in *
+line=`wc -l<$f`
+if [ $line -lt 10 ]
+MediumFiles="$MediumFiles $f"
+for ((i=0;i < $1;i++)) do done
+
+
+echo "$undergraduate" "$postgraduate" |sort| uniq|egrep ^$1
+
+
+#---------------------------
+# Tut 3
+# Good: 
+
+# Unsolved: 
+
+
+# Unattempted: 
+
+#===========================
+# Lab 4
+
+
+
+SAVEIFS=$IFS
+IFS=''
+IFS=$SAVEIFS
+
+
+Title=`echo $filename|sed 's/\(.*\) - \(.*\) - \(.*\)/\2/'`
+Artist=`echo $filename|sed 's/\(.*\) - \(.*\) - \(.*\)/\3/'|sed 's/.mp3//'`
+Title=`echo $filename|cut -d'/' -f3|sed 's/'-'/'#'/'|rev|sed 's/'-'/'#'/'|rev|cut -d'#' -f2|sed s/' '//`
+Artist=`echo $filename|cut -d'/' -f3|sed 's/'-'/'#'/'|rev|sed 's/'-'/'#'/'|rev|cut -d'#' -f3|sed s/' '//|sed s/'.mp3'//g`
+
+
+
+i=`expr $i + 1`
+
+
+
+
+#---------------------------
+# Tut 4
+# Good: 
+
+# Unsolved: 
+
+
+# Unattempted: 
+
+
+#===========================
 # Lab 5
+
+ord($byte) >= ord('0')
+chr(0x30)
+
+
+for($i=0;$i<$n;$i++) #do
+
+
+use POSIX;
+$index= floor(rand($num-$i));
+splice(@array, $index, 1);		# delete element at index
+
+
+seek F,0,0;
+
+
 
 #---------------------------
 # Tut 5
