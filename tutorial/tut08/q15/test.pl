84b8b2132278432861235c0304aa4255fdb8ca90
eudaemonal <eudaemonal@gmail.com>
2016-11-05 11:11:29 +1100

    update tut08

---
+++ tutorial/tut08/q15/test.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl -w
+
+
+@a = (1..5);
+@b = grep { $_ = $_ - 3; $_ > 0} @a;
+print "@a\n";
+print "@b\n";
+
---
+++ tutorial/tut08/q16/test.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl -w
+
+
+@vec = map { $_ ** 2 } (1,2,3,4,5);
+print "@vec\n";
+
+
